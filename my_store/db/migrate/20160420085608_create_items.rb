ca390e17e83085fb77c038b1e536b3b52ba0475b
urzp <urzp002@mail.ru>
2016-04-21 08:48:56 +0300

    end leel 2

--- my_store/db/migrate/20160420085608_create_items.rb
+++ my_store/db/migrate/20160420085608_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-	  t.flooat :prise  # новая	колонка  цена
+	  t.float :prise  # новая	колонка  цена
 	  t.string :name
 	  t.boolean :real
-	  t:float :weight
+	  t.float :weight
       t.timestamps  # специальный матод добовляет два столбца :created_at, updated_at будет фиксировать время создания и обновления модели
 	  
     end
