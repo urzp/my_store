936184570c738f187aae4525eb3cf473554475ec
urzp <urzp002@mail.ru>
2016-04-21 21:51:58 +0300

    level 8 (params) 1

--- my_store/app/controllers/items_controller.rb
+++ my_store/app/controllers/items_controller.rb
@@ -3,4 +3,10 @@ class ItemsController < ApplicationController
         @items= Item.all
         render text: @items.map {|i| "  #{i.name} price #{i.price}$"}.join("<br/>")
     end    
+    
+    def create
+        @item=Item.create(name: params[:name], description: params[:description], price: params[:price],real: params[:real], weight: params[:weight])
+        render text: "#{@item.id}  #{@item.name} (#{!@item.new_record?})" 
+    end
+    
 end
--- my_store/app/models/item.rb
+++ my_store/app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
     #attr_accessible :price, :name  # позволяте установливать поля черех хеш i=Item.new(price: 100, name: "new item")
     
     validates :price, { numericality: { greater_than: 0, allow_nil: true} } 
-    validates :price, :description, presence: true
+    validates :name, :price, :description, presence: true
     
     after_initialize { puts "initilazed"}
     after_save { puts "saved"}
