0c7548dde2b893dba55575fa160b4964a2970fde
urzp <urzp002@mail.ru>
2016-04-21 20:19:58 +0300

    end level 7 (callbacks)

--- my_store/app/models/item.rb
+++ my_store/app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ActiveRecord::Base
     #attr_accessible :price, :name  # позволяте установливать поля черех хеш i=Item.new(price: 100, name: "new item")
+    
+    validates :price, { numericality: { greater_than: 0, allow_nil: true} } 
+    validates :price, :description, presence: true
+    
+    after_initialize { puts "initilazed"}
+    after_save { puts "saved"}
+    after_create {puts "created"}
+    after_update {puts "updated"}
+    after_destroy {puts "destroyed"}
+    
 end
